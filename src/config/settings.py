d153bc04d7b3da43954c4baa3aca3ad318781044
KoiLab <dtc235200411@ictu.edu.vn>
2026-01-29 01:41:58 +0700

    Display total approved revenue in admin orders list

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     'users.apps.UsersConfig',   #Khai báo app users vào hệ thống
     'catalog', #Khai báo app catalog vào hệ thống
